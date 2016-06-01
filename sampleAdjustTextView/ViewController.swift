b8af47e3863934a2d464946508ad4e923e55ead1
ei17ringo <ei17ringo@gmail.com>
2016-06-01 08:49:14 +0800

    init commit

--- sampleAdjustTextView/ViewController.swift
+++ sampleAdjustTextView/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var adjustTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        adjustTextView.frame = CGRectMake(0, 20, 320, 460)
+        
+        var accessoryView = UIView(frame: CGRectMake(0, 0, 320, 44))
+        
+        accessoryView.backgroundColor = UIColor.whiteColor()
+        
+        var closeButton = UIButton(frame: CGRectMake(240, 0, 80, 30))
+        closeButton.setTitle("完了", forState: UIControlState.Normal)
+        closeButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        closeButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Right
+        closeButton.addTarget(self, action: "onClickCloseButton:", forControlEvents: .TouchUpInside)
+        accessoryView.addSubview(closeButton)
+        adjustTextView.inputAccessoryView = accessoryView
+        
+        self.view.addSubview(adjustTextView)
+    
+    }
+    
+    func textViewShouldBeginEditing(textView: UITextView) -> Bool {
+        adjustTextView.frame = CGRectMake(0, 20, 320, 200)
+        print("edit")
+        return true
+    }
+    
+    func onClickCloseButton(sender: UIButton) {
+        adjustTextView.resignFirstResponder()
+        
+        adjustTextView.frame = CGRectMake(0, 20, 320, 460)
+        
     }
 
     override func didReceiveMemoryWarning() {
